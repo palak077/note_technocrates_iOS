67037cc77ac38c746a6a952b10ae8a930383f91d
Palak Randhawa <palakrandhawa0804@gmail.com>
2021-01-27 18:09:49 -0500

    Initial Commit

---
+++ note_technocrates_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  note_technocrates_iOS
+//
+//  Created by Macbook on 2021-01-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
